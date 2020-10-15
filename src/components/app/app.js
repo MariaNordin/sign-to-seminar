902a9ed9cac48adb804488d71d71e021d9976629
Maria Nordin <miamarianordin@gmail.com>
2020-10-15 13:37:35 +0200

    Correcting links

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './app.css';
 
 import Header from './../header/header';
-import Logo from './../header/logo/logo';
 import MenuLink from './../header/menuLink/menuLink';
 import Body from './../body/body';
 import Footer from './../footer/footer';
@@ -14,7 +13,6 @@ export default class App extends Component {
       <>
         <Router>
           <Header>
-            <Logo />
             <MenuLink link='/' name='HOME' />
             <MenuLink link='/upcoming' name='UPCOMING SEMINARS' />
             <MenuLink link='/previous' name='PREVIOUS SEMINARS' />
--- src/components/header/menuLink/menuLink.js
+++ src/components/header/menuLink/menuLink.js
@@ -4,24 +4,10 @@ import { Link } from 'react-router-dom';
 export default class MenuLink extends Component {
     render() {
         return (
-
-            <div id="menu">
-                <ul>
-                    <li>
-                        <Link to={this.props.link}>{this.props.name}</Link>
-                    </li>
-                </ul>
-            </div>
-            /*<li>
-                <span onClick={() => this.props.onClick(this.props.name)}>
-                    {this.props.name}
-                </span>;
-            </li>*/
+            <Link to={this.props.link}>{this.props.name}</Link>
         );
     }
 }
-//Här ska jag länka till rätt sida ?
-
 
 //import React from 'react'
 

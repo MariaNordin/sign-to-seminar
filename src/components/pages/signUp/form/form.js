7c3ede1973cd9bbc1056d6e6a79eb2a475dd5ba4
Maria Nordin <miamarianordin@gmail.com>
2020-10-21 15:36:39 +0200

    SignUp is now a class, not a function

--- src/components/pages/signUp/form/form.js
+++ src/components/pages/signUp/form/form.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
-    constructor() {
-        super();
-        this.state = { name: '', email: '', seminarId: 0, userId: 0 }
+    constructor(props) {
+        super(props);
+        this.state = { name: '', email: '', userId: 0 }
     }
+    /*componentDidMount() {
+        this.fetchUsers();
+    }
+    componentDidUpdate(prevProp) {
+        if(this.props.users !== prevProp.users) this.fetchUsers();
+    }
+    fetchUsers() {
+        fetch('https://localhost:44346/api/Users')
+        .then((response) => response.json())
+        .then((json) => this.setState({ users: json }));
+    }*/
     saveSignUp() {
         fetch('https://localhost:44346/api/Users', {
             method: 'POST',
             body: JSON.stringify({
                 name: this.state.name,
                 email: this.state.email
-            })
-        })
+            }),
+            headers: { 'Content-type': 'application/json; charset=UTF-8'}
+        });
     }
+    /*catchId() {
+        this.state.users.forEach(element => {
+            if(element.name === this.state.name && element.email === this.state.email) {
+                this.setState({ userId: element.id });
+                }
+        });
+    }*/
     render() {
         return (
             <>
--- src/components/pages/signUp/signUp.js
+++ src/components/pages/signUp/signUp.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import InfoBox from './infoBox/infoBox';
 import Form from './form/form';
 import './signUp.css';
 
-export default function SignUp() {
-    return (
-        <>
-            <section id="form-section">   
-                <InfoBox />
-                <Form />
-            </section>            
-        </>
-    );
+export default class SignUp extends Component {
+    render() {
+        return (
+            <>
+                <section id="form-section">   
+                    <InfoBox />
+                    <Form id={this.props.id} />
+                </section>            
+            </>
+        )
+    }
 }

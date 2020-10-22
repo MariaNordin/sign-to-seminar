47eac717a5a4eddba0b849c42bb84dd3f0e04e1a
Maria Nordin <miamarianordin@gmail.com>
2020-10-22 22:20:08 +0200

    Sign Up now adds new/updates User
    And binds it to the correct seminar (userSeminars)

--- src/components/pages/signUp/form/form.js
+++ src/components/pages/signUp/form/form.js
@@ -1,54 +1,49 @@
 import React, { Component } from 'react'
 
+import './../signUp.css';
+
 export default class Form extends Component {
     constructor(props) {
         super(props);
-        this.state = { name: '', email: '', userId: 0 }
+        this.state = { name: '', email: '' }
     }
-    /*componentDidMount() {
-        this.fetchUsers();
-    }
-    componentDidUpdate(prevProp) {
-        if(this.props.users !== prevProp.users) this.fetchUsers();
-    }
-    fetchUsers() {
-        fetch('https://localhost:44346/api/Users')
-        .then((response) => response.json())
-        .then((json) => this.setState({ users: json }));
-    }*/
+    
     saveSignUp() {
-        fetch('https://localhost:44346/api/Users', {
+        
+        fetch('https://localhost:44346/api/Users', { 
             method: 'POST',
             body: JSON.stringify({
                 name: this.state.name,
-                email: this.state.email
+                email: this.state.email,
+                seminarId: this.props.id,
             }),
             headers: { 'Content-type': 'application/json; charset=UTF-8'}
         });
+        /*
+        .then((response) => response.json())
+        .then((json) => this.setState({ message: json }));*/
     }
-    /*catchId() {
-        this.state.users.forEach(element => {
-            if(element.name === this.state.name && element.email === this.state.email) {
-                this.setState({ userId: element.id });
-                }
-        });
-    }*/
+    
     render() {
         return (
             <>
+                <div className="form-box">
                 Name:
-                <input 
-                    type = 'text'
-                    value = {this.state.name}
-                    onChange = {(e) => this.setState({ name: e.target.value })}
-                />
+                    <input 
+                        type = 'text'
+                        value = {this.state.name}
+                        onChange = {(e) => this.setState({ name: e.target.value })}
+                    />
+                </div>
+                <div className="form-box">
                 Email:
-                <input
-                    type = 'text'
-                    value = {this.state.email}
-                    onChange = {(e) => this.setState({ email: e.target.value })}
-                />
-                <button onClick = {() => this.saveSignUp()}>Sign Up!</button>
+                    <input
+                        type = 'text'
+                        value = {this.state.email}
+                        onChange = {(e) => this.setState({ email: e.target.value })}
+                    />
+                </div>
+                <button id="submit" onClick = {() => this.saveSignUp()}>Sign Up!</button>               
             </>
         );
     }

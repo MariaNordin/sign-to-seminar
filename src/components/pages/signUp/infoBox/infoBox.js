48d650ed35fd69cadb620a2f1705c41b5c5f0ca5
Maria Nordin <miamarianordin@gmail.com>
2020-10-23 20:45:10 +0200

    InfoBox now render correct Seminar date
    Still needs formatting

--- src/components/pages/signUp/infoBox/infoBox.js
+++ src/components/pages/signUp/infoBox/infoBox.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 export default class InfoBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { seminar: null }
+    }
+    componentDidUpdate(prevProp) {
+        if(this.props.id !== prevProp.id) this.fetchSeminar();
+    }
+    fetchSeminar() {
+        fetch('https://localhost:44346/api/Seminars/' + this.props.id)
+        .then((response) => response.json())
+        .then((json) => this.setState({ seminar: json }));
+    }
     render() {
         return (
             <> 
-                <p>Date: January 2020
-                <br/>Time: 5pm
-                <br/>Place: Street City
+                {this.state.seminar != null ? (
+                <p>Date: {this.state.seminar.day.day}
+                <br/>Time: 
                 </p>
+                ) : null}
             </>
         )
     }

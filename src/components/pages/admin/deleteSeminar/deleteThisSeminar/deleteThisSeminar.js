f95808ccc3137303b7e54375ba1e9ec78e7ee319
Maria Nordin <miamarianordin@gmail.com>
2020-10-29 01:37:05 +0100

    Delete works ok

--- src/components/pages/admin/deleteSeminar/deleteThisSeminar/deleteThisSeminar.js
+++ src/components/pages/admin/deleteSeminar/deleteThisSeminar/deleteThisSeminar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import InfoBox from './../../../signUp/infoBox/infoBox';
-import cup from './../../../../../images/cup.svg';
 
 export default class DeleteThisSeminar extends Component {
     constructor() {
@@ -16,18 +15,40 @@ export default class DeleteThisSeminar extends Component {
         this.fetchSeminar();
     }
     fetchSeminar() {
-        fetch(fetch('https://localhost:44346/api/Seminars/' + this.props.id)
+        fetch('https://localhost:44346/api/Seminars/' + this.props.id)
         .then((response) => response.json())
-        .then((json) => this.setState({ seminar: json })))
+        .then((json) => this.setState({ seminar: json }));
+    }
+    deleteSeminar() {
+        fetch('https://localhost:44346/api/Seminars/' + this.props.id, {
+            method: 'DELETE',
+            body: JSON.stringify({
+                name: this.state.name,
+                description: this.state.description,
+                SpeakersName: this.state.speakersName,
+                Date: this.state.date,
+                Time: this.state.time
+            }), 
+            headers: { 'Content-type': 'application/json; charset=UTF-8'}
+        })
+        .then((response) => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw 'Something went wrong';
+            }
+        })
+        .then((json) => this.setState({ message: json.message })
+        );
     }
     render() {
         return (
             <div>
                 {this.state.seminar !== null ? (
                     <div>
-                            <h3>Delete {this.state.seminar.name}?</h3>
-                            <InfoBox date={this.state.seminar.day.day} />
-                            <img src={cup} alt="cup" />
+                        <h3>Delete {this.state.seminar.name}?</h3>
+                        <InfoBox date={this.state.seminar.day.day} />
+                        <button id='deleteButton' onClick={() => this.deleteSeminar()}>Delete</button>
                     </div>   
                 ): this.fetchSeminar()}
             </div>

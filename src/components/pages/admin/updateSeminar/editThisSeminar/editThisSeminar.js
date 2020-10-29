d3b38826c37a775b8e1b44aea10180fa12d7c8cb
Maria Nordin <miamarianordin@gmail.com>
2020-10-29 02:27:02 +0100

    Edit still not working

--- src/components/pages/admin/updateSeminar/editThisSeminar/editThisSeminar.js
+++ src/components/pages/admin/updateSeminar/editThisSeminar/editThisSeminar.js
@@ -4,7 +4,15 @@ import React, { Component } from 'react';
 export default class EditThisSeminar extends Component {
     constructor() {
         super();
-        this.state = { seminar: null, name: '', description: '', speakersName: '', date: '', time: '' }
+        this.state = { 
+            seminar: null, 
+            name: '', 
+            description: '', 
+            speakersName: '', 
+            date: '', 
+            time: '', 
+            message: ''
+        }
     }
     componentDidMount() {
         this.fetchSeminar();
@@ -20,20 +28,42 @@ export default class EditThisSeminar extends Component {
                 .then((json) => this.setState({ seminar: json }));
         }
     }
-    handleDateTime(date, time) {
-        var testStr = date + time;
-        const dateTimeRegex = /\d{4}\/\d{2}\/\d{4}\:\d{2}/;
-        (dateTimeRegex.test(testStr)) ? (
-            this.updateSeminar()
-        ):
-        this.setState({ message: 'Date or Time format is incorrect'})
-        }
+    checkValues() {
+        if(this.state.name === '') 
+            this.setState({ name: this.state.seminar.name });
+        if(this.state.description === '') 
+            this.setState({ description: this.state.seminar.description });
+        if(this.state.speakersName === '') 
+            this.setState({ speakersName: this.state.seminar.speaker.name });
+        if(this.state.date === '') 
+            this.setState({ date: this.state.seminar.day.day.substring(0, 10) })
+        if(this.state.time === '') 
+            this.setState({ time: this.state.seminar.day.day.substring(11, 16) })
+        this.updateSeminar();
+    }
     updateSeminar() {
-        
+        fetch('https://localhost:44346/api/Seminars/' + this.state.seminar.id, {
+                method: 'PUT',
+                body: JSON.stringify({
+                name: this.state.name,
+                description: this.state.description,
+                SpeakersName: this.state.speakersName,
+                Date: this.state.date,
+                Time: this.state.time
+            }), 
+            headers: { 'Content-type': 'application/json; charset=UTF-8'}
+        })
+        .then((response) => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw 'Something went wrong';
+            }
+        });
     }
     render() {
         return (
-            <div id='edit-container'>
+            <div>
                 {this.state.seminar !== null ? (
                 <div key={this.state.seminar.id}>
                     <div id='title' >Edit Seminar</div>
@@ -41,7 +71,7 @@ export default class EditThisSeminar extends Component {
                         Title: 
                         <input
                             type='text'
-                            value={this.state.seminar.name}
+                            defaultValue={this.state.seminar.name}
                             onChange={(e) => this.setState({ name: e.target.value })}
                         />
                         Description: 
@@ -69,7 +99,7 @@ export default class EditThisSeminar extends Component {
                             onChange={(e) => this.setState({ time: e.target.value })}
                         />
                     </div>
-                    <button onClick={() => this.handleDateTime(this.state.date, this.state.time)}>Save</button>
+                    <button id='updateButton' onClick={() => this.checkValues()}>Save</button>
                     <div id="message">{this.state.message}</div>   
                 </div>
                 ): this.fetchSeminar() }
